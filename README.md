75ec870a816f68c6fce0fa4ba92ae62c61330f78
cristiansep <cristiansepulvedamendez@gmail.com>
2020-03-15 14:56:20 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Webpack Starter
+
+Proyecto inicial para crear aplicaciones utilizando webpack.
+
+### Notas:
+Para recosntruir los modulos de Node 
+```
+npm install
+```
+
+Y para reconstruir el build, escribir el comando:
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWepackPlugin = require('html-webpack-plugin');
+const MiniCssPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    mode: 'development',
+    optimization: {
+        minimizer: [ new OptimizeCssAssetsPlugin() ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/img/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+        plugins: [
+            new HtmlWepackPlugin({
+                template: './src/index.html',
+                filename: './index.html'
+            }),
+            new MiniCssPlugin({
+                filename: '[name].css',
+                ignoreOrder: false
+            }),
+            new CleanWebpackPlugin(),
+        ]
+
+}
+
